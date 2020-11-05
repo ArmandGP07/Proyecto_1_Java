c23d57c5a6e8b9812e04aefbfd3f001472f73e69
Armando García Paniagua <62965639+ArmandGP07@users.noreply.github.com>
2020-11-04 19:03:05 -0600

    GUI
    Se creó el GUI para el juego usando Java Swing UI Designer y se hizo un intento de un Drag and Drop para el manejo de las cartas.

--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,95 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Deck {
+
+    private Nodo inicio;
+    private int tamagno;
+
+    public Deck() throws IOException {
+        int n = 16;
+        ArrayList<Cartas> cartas = Serializacion.getListaCartas();
+
+        for(int i = 0; i<16;i++) {
+            int numero = (int) (Math.random() * n) + 1;
+            agregarAlFinal(cartas.get(numero));
+        }
+    }
+
+    public int getTamagno() {
+        return tamagno;
+    }
+
+    public boolean esVacia(){
+        return inicio == null;
+    }
+
+
+    public void agregarAlFinal(Cartas valor) {
+
+        Nodo nuevo = new Nodo();
+        nuevo.setValor(valor);
+
+        if (esVacia()) {
+            inicio = nuevo;
+
+        } else if(tamagno==1){
+            inicio.setNext(nuevo);
+            inicio.setPrev(nuevo);
+            nuevo.setNext(inicio);
+            nuevo.setPrev(inicio);
+        }
+        else {
+            Nodo aux = inicio;
+
+            while (aux.getNext() != inicio) {
+                aux = aux.getNext();
+            }
+            aux.setNext(nuevo);
+            nuevo.setPrev(aux);
+            nuevo.setNext(inicio);
+            inicio.setPrev(nuevo);
+        }
+        tamagno++;
+    }
+
+    public void listar(){
+        // Verifica si la lista contiene elementoa.
+        if (!esVacia()) {
+            Nodo aux = inicio;
+            int i = 0;
+            System.out.println("Las cartas del deck son ");
+
+            while(aux != null && i<tamagno){
+                System.out.print(String.format("[ %s ]", aux.getValor().getNombre()));
+
+                if (i!=tamagno-1) {
+                    System.out.print(" <-> ");
+                }
+                if((i+1)%2==0){
+                    System.out.println("");
+                }
+                aux = aux.getNext();
+                i++;
+            }
+        }
+    }
+
+    public Nodo getNodo(int posición){
+        Nodo aux = inicio;
+
+        int residuo = posición % getTamagno();
+        //System.out.println(residuo);
+
+        if (residuo == 0){
+            return aux;
+        } else {
+            for (int i = 0; i < residuo; i++) {
+                aux = aux.getNext();
+            }
+        }
+        return aux;
+    }
+
 
 }
--- src/Esbirros.java
+++ src/Esbirros.java
@@ -6,7 +6,65 @@ public class Esbirros extends Cartas {
         this.ataque=ataque;
     }
 
-    public void Atacar (Usuario player){
-        player.setHp(player.getHp()-ataque);
+    public Esbirros(){}
+
+
+    @Override
+    public void UsarCarta(Usuario usuario, Usuario enemigo) {
+
+        switch(getNombre()){
+            case "Slime":{
+                System.out.println(String.format("Soy un slime y ataqué a %s", enemigo.getNombre()));
+                enemigo.setHp(enemigo.getHp()-ataque);
+                System.out.println(String.format("Le hice %d de daño a %s, ahora su hp es de %d", getAtaque(),enemigo.getNombre(),enemigo.getHp()));
+                break;
+            }
+            case "Minotauro":{
+                System.out.println("Soy un minotauro");
+                break;
+            }
+            case "Mago":{
+                System.out.println("Soy un mago, merlín");
+                break;
+            }
+            case "Bruja":{
+                System.out.println("Soy una bruja");
+                break;
+            }
+            case "Caballero":{
+                System.out.println("Soy un caballero y derrotaré al enemigo");
+                break;
+            }
+            case "Valkiria":{
+                System.out.println("Soy una valkiria");
+                break;
+            }
+            case "Dragón":{
+                System.out.println("Soy un dragón");
+                break;
+            }
+            case "Horda de duendes":{
+                System.out.println("Somos unos duendes");
+                break;
+            }
+            case "Esqueletos":{
+                System.out.println("Somos esqueletos, morimos hace mucho");
+                break;
+            }
+            case "Cerbero":{
+                System.out.println("Soy Cerbero, tengo 3 cabezas");
+                break;
+            }
+
+        }
+    }
+
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
     }
 }
